441b55486f98518617495a2c9d0c0040f0868556
Yeimer Molina Ramirez <Yeimer.Molina@endava.com>
2020-07-20 12:49:18 -0500

    Add initial files

---
+++ lib/models/product.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final int qty;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.qty = 0,
+    this.isFavorite = false,
+  });
+}
